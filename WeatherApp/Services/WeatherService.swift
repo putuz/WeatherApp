35dea1b61c6a4433d518fe6ef38d9f5c86f7a73c
putuz <pututyb@gmail.com>
2025-01-11 12:42:28 +0700

    api key.

--- WeatherApp/Services/WeatherService.swift
+++ WeatherApp/Services/WeatherService.swift
@@ -9,7 +9,7 @@ import Foundation
 import Combine
 
 class WeatherService {
-    private let apiKey = "ea0d28a11e2124a89d4870e893f4ee6e"
+    private let apiKey = "ea0d28a11e2124a89d4870e893f4ee6e" // change with your custom API.
     
     func fetchWeather(lat: Double, lon: Double) -> AnyPublisher<WeatherResponse, Error> {
         let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&units=metric&appid=\(apiKey)"
